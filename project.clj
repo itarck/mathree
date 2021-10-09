437438541ca9acf99bb33efe3ec3ae7ec6238b9d
itarck <itarck@gmail.com>
2021-10-09 16:02:38 +0800

    Fix dependencies in project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
-(defproject com.github.itarck/mathree "0.1.0-SNAPSHOT"
+(defproject com.github.itarck/mathree "0.1.1-SNAPSHOT"
   :description "ClojureScript wrapper for Threejs math library"
   :url "https://github.com/itarck/mathree"
   :license {:name "MIT License"
-            :url  "https://opensource.org/licenses/MIT"})
+            :url  "https://opensource.org/licenses/MIT"}
+  :source-paths ["src"]
+  :dependencies [[applied-science/js-interop "0.3.1"]])
